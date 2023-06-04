ddb39b1a1d7b78d1b748e8b13539c88e591c665a
Juan Manuel <114026096+JuanManuel1970@users.noreply.github.com>
2023-06-04 08:29:41 -0300

    deploy back

---
+++ model/model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const { Schema } = require('mongoose');
+
+//configuramos con Schema nuestra coleccion de database
+const userSchema = new Schema({
+   nombre:{ 
+    type : String,
+    required: true
+   },
+   apellido:{ 
+    type : String,
+    required: true
+   },
+   email:{ 
+    type: String,
+    required: true
+   },
+   password:{
+    type: String,
+    required: true
+   },
+   timestamp:{ 
+    type : Date,
+    default: new Date(),
+
+}
+
+});
+//exportamos la configuracion con el nombre de la coleccion
+
+module.exports = mongoose.model('Formularios', userSchema);
+
+
